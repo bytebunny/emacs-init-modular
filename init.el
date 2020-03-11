8cdaeba2da9239f26045ea16a2ee5996e7742a75
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2020-03-11 22:44:28 +0100

    Added starting in full screen.

--- init.el
+++ init.el
@@ -59,3 +59,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(add-to-list 'default-frame-alist '(fullscreen . fullscreen)) ; Start fullscreen mode.
