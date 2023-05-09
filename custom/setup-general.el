f59679384e7087d117dc778ece8f61ff69f997b6
Rostyslav Skrypnyk <bytebunny@sandbox.sandbox.local>
2023-05-10 00:41:49 +0200

    Keep company-semantic backend.

--- custom/setup-general.el
+++ custom/setup-general.el
@@ -36,7 +36,8 @@
 (use-package company
   :config
   (add-hook 'after-init-hook 'global-company-mode)
-  (delete 'company-semantic company-backends))
+  ;;(delete 'company-semantic company-backends)
+  )
 ;; (define-key c-mode-map  [(control tab)] 'company-complete)
 ;; (define-key c++-mode-map  [(control tab)] 'company-complete)
 
